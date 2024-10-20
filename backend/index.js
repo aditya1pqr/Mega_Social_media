dfef822cfb8763281e5293e002427bcbfa9a8dc6
Aditya <aadi099012@gmail.com>
2024-10-20 23:41:14 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,27 @@
-import express from "express"
+import express, { json } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
 const app =  express();
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+   
+   return  res.status(200).json({
+        message: "Hello i am from backend",
+        success: true
+    })
+})
+const corsOptions = {
+    origin: "http://localhost:5173",
+    Credential:true
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.listen(PORT,()=>{
+    console.log(`server is listening at ${PORT}`);
 
 })
