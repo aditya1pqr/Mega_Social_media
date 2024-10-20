d4a42796770589a3a7438b0b2f85c19b31f2ba3a
Aditya <aadi099012@gmail.com>
2024-10-20 23:53:32 +0530

    error resolved

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
+const app =  express();
+const PORT = 8000;
+
+app.get('/',(req,res)=>{
+   
+   return  res.status(200).json({
+        message: "Hello i am from backend",
+        success: true
+    })
+})
+const corsOptions = {
+    origin: "http://localhost:5173",
+    Credential:true
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+
+app.listen(PORT,()=>{
+
+    console.log(`server is listen at port ${PORT}`);
+
+})
